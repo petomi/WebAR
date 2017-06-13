121f2096b16dc9b755b4e25f350383f16fce7af6
Michael Peto <michael.peto10@gmail.com>
2017-06-13 15:23:44 -0400

    initial commit from ARApp2

---
+++ libs/threex.videotexture.js
@@ -0,0 +1,35 @@
+var THREEx = THREEx || {}
+
+THREEx.VideoTexture	= function(url){
+	// create the video element
+	var video	= document.createElement('video');
+	video.width	= 320;
+	video.height	= 240;
+	video.autoplay	= true;
+	video.muted = true; //added to allow muted autoplay on mobile chrome
+	video.loop	= true;
+	video.crossOrigin='anonymous';
+	video.src	= url;
+	// expose video as this.video
+	this.video	= video
+
+	// create the texture
+	var texture	= new THREE.Texture( video );
+	// expose texture as this.texture
+	this.texture	= texture
+
+	/**
+	 * update the object
+	 */
+	this.update	= function(){
+		if( video.readyState !== video.HAVE_ENOUGH_DATA )	return;
+		texture.needsUpdate	= true;
+	}
+
+	/**
+	 * destroy the object
+	 */
+	this.destroy	= function(){
+		video.pause()
+	}
+}
